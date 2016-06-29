20a2e8aff4483921a0093177e37cd941920534eb
Avi Areman <aareman000@citymail.cuny.edu>
2016-06-29 19:35:20 -0400

    added controls for sketch
    todo: add a water spray to make it more realistic in terms of why the wheel is not sliding.

--- examples/rotating_wheel/sketch.js
+++ examples/rotating_wheel/sketch.js
@@ -14,9 +14,34 @@ function setup(){
     // "on" is used for the pause and resume at the end of file
     // that allows for stopping/starting the sketch w/o prob.
     on = true;
+
+
+    //create controls for sketch
+    rotate_speed = createSlider(.1,5,1,.2);
+    rotate_speed.position(20,60)
+
+    btn_pause = createButton('Pause');
+    btn_pause.position(20,100);
+    btn_pause.mouseClicked(ptoggle);
+
+    //particles for water spin
+    var t =
+    {
+        name: "test",
+        colors: ["blue",[0,255,127,127],[0,255,64,32]],
+        lifetime: 600,
+        angle: [330,360],
+        x: 0.2,
+        y: 0.1
+    };
+    of = new Fountain(null,t);
+    //TODO download correct particle lib not grafica!!!
 }
 function draw(){
     background(121,209,254);
+
+    //draw text for controls
+    text('Rotation Speed',20,40);
 
     //draw ground
     noStroke();
@@ -24,14 +49,23 @@ function draw(){
     rect(0,w.y+w.r,width,height);
     //draw the wheel.
     w.draw();    
+    //get speed from slider
+    w.ang_speed = rotate_speed.value();
+
+    //draw particles
+    of.Draw();
+    of.Create();
+    of.Step();
 }
-function mouseClicked(){
+function ptoggle(){
     if(on == true){
         on = false;
         noLoop();
+        btn_pause.html('Play');
     }
     else{
         on = true;
         loop();
+        btn_pause.html('Pause');
     }
 }
