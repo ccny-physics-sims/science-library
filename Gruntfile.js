cb94dd11816542e2bdf88f78ae77d13026906778
hedbergj <james@smile2.local>
2016-09-27 22:12:21 -0400

    zap physgraphs

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
         '<%= grunt.template.today("yyyy-mm-dd") %> */ \n',
       },
       dist: {
-        src:['src/arrow.js','src/KineticMass.js','src/mover.js','src/spring.js','src/axes.js','src/background-motion.js','src/wheel.js','src/fbd.js','src/physgraphs.js'],
+        src:['src/arrow.js','src/KineticMass.js','src/mover.js','src/spring.js','src/axes.js','src/background-motion.js','src/wheel.js','src/fbd.js'],
         dest: 'lib/science.js',
       },
     },
--- src/axes.js
+++ src/axes.js
@@ -10,8 +10,8 @@ function drawAxes(lineColor,thickness){
   push();
   this.lineColor = 'black';
   this.thickness = 1;
-  stroke(lineColor)
-  strokeWeight(thickness);
+  stroke(this.lineColor)
+  strokeWeight(this.thickness);
   line(width/2,0,width/2,height)
   line(0,height/2,width,height/2)
   pop();
