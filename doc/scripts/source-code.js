b20334fe7c829ef914f9f0983eeb5f4468874af9
C <dreifarbenralle@gmail.com>
2017-05-16 21:07:40 -0400

    removed some debugging leftovers

--- doc/scripts/source-code.js
+++
@@ -1,8 +0,0 @@
-
-var editor;
-$('.editor').each(function( index ) {
-    editor = ace.edit(this);
-	  editor.getValue(this);
-      // editor.setTheme("ace/theme/monokai");
-    // editor.getSession().setMode("mode-javascript");
-});
